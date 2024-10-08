d0a3c6f1306b7b8c1cc6efc8c04b4ed9b709b879
Zuruel Kamande <zurueladrian@gmail.com>
2024-10-08 09:36:27 +0300

    yhfyuf

---
+++ index.js
@@ -0,0 +1,27 @@
+const foo = function() {
+    return 'bar';
+  }
+
+  const add = (a, b) => a + b;
+
+  const sum = (a, b) => {
+    console.log(a, b);
+    return a + b;
+  };
+
+  // Function expression for divide
+const divide = function() {
+    return 2000 / 100;
+  };
+  
+  // Arrow function for square
+  const square = (num) => num * num;
+
+  const nums = [1, 2, 3];
+const squares = nums.map(x => x ** 2);
+
+
+
+
+
+
